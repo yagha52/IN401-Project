8ae17ee1327777c91878c6f33cbcc438c0cf6cde
YaRa <eslimyara2@gmail.com>
2025-05-28 08:53:52 +0300

    fixed: Update Inventory

--- Models/Inventory.cs
+++ Models/Inventory.cs
@@ -2,9 +2,7 @@
 {
     public class Inventory
     {
-        public int InventoryID { get; set; }
         public int ProductID { get; set; }
-        public Product Product { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
     }
